69b769014617abe21658f975d516f4b0a5decdeb
carlojacob <carlo.jacob@gmail.com>
2019-01-29 18:51:39 -0500

    Add Relationship between Resources
      - Generate migration file to add `user_id` to `rides` table.
      - Migrate `user_id` column into `rides` table.
      - Add relationships to `ride` and `user` models.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_29_202944) do
+ActiveRecord::Schema.define(version: 2019_01_29_232053) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,8 @@ ActiveRecord::Schema.define(version: 2019_01_29_202944) do
     t.float "ride_distance", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_rides_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -43,4 +45,5 @@ ActiveRecord::Schema.define(version: 2019_01_29_202944) do
   end
 
   add_foreign_key "examples", "users"
+  add_foreign_key "rides", "users"
 end
